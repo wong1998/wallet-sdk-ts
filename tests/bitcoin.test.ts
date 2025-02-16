d6de23c6ac7a575c255224f39fa9087dca217225
wong1998 <897068715@qq.com>
2025-02-16 23:34:15 +0800

    1

---
+++ tests/bitcoin.test.ts
@@ -0,0 +1,17 @@
+import {createBTCAddress, mnemonicToSeed} from "../src/bitcoin/bitcoin";
+
+
+describe('btc unit test case', () => {
+    test('createAddress', () => {
+        const mnemonic = "install million poem gloom timber equal way cash spread lobster spring depth";
+        const params_1 = {
+            mnemonic: mnemonic,
+            password: ""
+        }
+        const seed = mnemonicToSeed(params_1)
+        const account = createBTCAddress(seed.toString("hex"), "0", "0", "mainnet")
+        console.log(account)
+    //  bc1qs6zjd5jfn6a37ffkdfnmhd2lm4ykg8uhyjh0qy
+    });
+
+});
